f6f112651b915091477f4fdaa4fb6b56a2dc4560
danielyyi <daniel.youngsun@gmail.com>
2021-08-14 19:57:53 -0400

    please work

--- functions/fetch-hypixel.js
+++ functions/fetch-hypixel.js
@@ -1,12 +1,12 @@
-import fetch from 'node-fetch'
+//import fetch from 'node-fetch'
 exports.handler = async (event, context) => {
-  const uuid = event.queryStringParameters.uuid
-  const key = process.env.REACT_APP_API_KEY;
+  //const uuid = event.queryStringParameters.uuid
+  //const key = process.env.REACT_APP_API_KEY;
 
-  const rawRes = await fetch(
-   `https://api.hypixel.net/player?key=ef0859e4-11c3-4f46-a665-e7607f4c702e&uuid=${uuid}`
-  );
-  const res = await rawRes.json();
+  //const rawRes = await fetch(
+  // `https://api.hypixel.net/player?key=ef0859e4-11c3-4f46-a665-e7607f4c702e&uuid=${uuid}`
+  //);
+  //const res = await rawRes.json();
   return {
     statusCode: 200, 
     headers: {
@@ -14,7 +14,7 @@ exports.handler = async (event, context) => {
       'Access-Control-Allow-Headers':
         'Origin, X-Requested-With, Content-Type, Accept'
     },
-    body: JSON.stringify(res)
+    body: JSON.stringify("hi")
   }
 }
 
