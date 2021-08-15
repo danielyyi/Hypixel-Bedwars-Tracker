67d249b8af3229654c1b3d19f4919036409897ec
danielyyi <daniel.youngsun@gmail.com>
2021-08-14 20:58:34 -0400

    vyulblyuiilybu

--- functions/fetch-hypixel.js
+++ functions/fetch-hypixel.js
@@ -1,12 +1,12 @@
-import fetch from 'node-fetch'
+const fetch = require("node-fetch");
 exports.handler = async (event, context) => {
   const uuid = event.queryStringParameters.uuid
   const key = process.env.REACT_APP_API_KEY;
   const hi = "Hi Daniel"
-  //const rawRes = await fetch(
-   //`https://api.hypixel.net/player?key=${key}&uuid=${uuid}`
-  //);
-  //const res = await rawRes.json();
+  const rawRes = await fetch(
+   `https://api.hypixel.net/player?key=${key}&uuid=${uuid}`
+  );
+  const res = await rawRes.json();
   return {
     statusCode: 200, 
     headers: {
